eb03785516a9ffc134d9b1d01657f5fe68146c63
caraclarke <cara.clarke8@gmail.com>
2016-10-18 12:26:30 -0400

    update template and add corresponding methods

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -17,12 +17,12 @@ export class ProductList {
   /*
   * @ input productList: the product array passed to us
   */
-  productList: Product[];
+  public productList: Product[];
 
   /*
   * @output onProductSelected - outputs current product whenever new product is selected
   */
-  onProductSelected: EventEmitter<Product>;
+  public onProductSelected: EventEmitter<Product>;
 
   /*
   * @property currentProduct - local state containing the currently selected product
@@ -31,5 +31,26 @@ export class ProductList {
 
   constructor() {
     this.onProductSelected = new EventEmitter();
+  }
+
+  /*
+  * set this.currentProduct to the product passed in
+  * emit product that was clicked on our output
+  */
+  public clicked(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  /*
+  * accepts Product and returns true if products sku matches currentProduct sku
+  * otherwise returns false
+  */
+  public isSelected(product: Product): boolean {
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+
+    return product.sku === this.currentProduct.sku;
   }
 }
