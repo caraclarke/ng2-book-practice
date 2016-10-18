56cfed06ab9a8024dc0aa88b3b53688abcc6a2a2
caraclarke <cara.clarke8@gmail.com>
2016-10-18 12:32:34 -0400

    add comments, inputs and host

--- src/app/product/product-row/product-row.component.ts
+++ src/app/product/product-row/product-row.component.ts
@@ -1,8 +1,14 @@
 import { Component, EventEmitter } from "@angular/core";
 import { Product } from "../../models/product";
 
+/*
+* component for view of single product
+*/
+
 @Component({
   selector: "product-row",
+  inputs: ["product"],
+  host: {"class": "item"},
   templateUrl: "./product-row.component.html"
 })
 
