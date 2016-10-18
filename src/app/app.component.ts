a2e14f4f457a5a64468981fb0c8ed5cc71c8fce5
caraclarke <cara.clarke8@gmail.com>
2016-10-18 11:33:15 -0400

    fake data array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewEncapsulation } from "@angular/core";
 
+import { Product } from "./models/product";
+
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: "my-app",
@@ -7,4 +9,34 @@ import { Component, ViewEncapsulation } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 
-export class AppComponent { }
+export class AppComponent {
+
+  public products: Product[];
+
+  constructor() {
+    this.products = [
+      {
+        sku: 'MYSHOES',
+        name: 'Black Running Shoes',
+        imageUrl: '/resources/images/products/black-shoes.jpg',
+        department: ['Men', 'Shoes', 'Running Shoes'],
+        price: 109.99
+      },
+      {
+        sku: 'NEATOJACKET',
+        name: 'Blue Jacket',
+        imageUrl: '/resources/images/products/blue-jacket.jpg',
+        department: ['Women', 'Apparel', 'Jackets & Vests'],
+        price: 238.99
+      },
+      {
+        sku: 'NICEHAT',
+        name: 'A Nice Black Hat',
+        imageUrl: '/resources/images/products/black-hat.jpg',
+        department: ['Men', 'Accessories', 'Hats'],
+        price: 29.99
+      }
+    ];
+  }
+
+}
